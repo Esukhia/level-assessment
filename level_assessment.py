a81d87639b84adddbddc30593c6e04e23a4472d7
kaldan007 <kaldantenzin@gmail>
2021-07-22 12:00:57 +0530

    segmentation and postagger added

--- level_assessment.py
+++ level_assessment.py
@@ -5,6 +5,7 @@ from annotated_text import annotation
 from botok import WordTokenizer
 from htbuilder import H, HtmlElement
 
+st.set_page_config(layout='wide')
 """
 # Word Level Assessment Tool For Tibetan Language
 
@@ -30,6 +31,12 @@ level_code_lookup = {
     'l3': '#faa'
 }
 
+pos_ann = {
+    'VERB': '#8ef',
+    'NOUN': '#afa',
+    'PART': '#faa'
+}
+
 def get_tokens(text):
     tokens = wt.tokenize(text, split_affixes=False)
     return tokens
@@ -43,26 +50,39 @@ def check_level(word):
     return level, level_code
 
     
-def assess_level(input_text):
-    tokens = get_tokens(input_text)
-    level_annotated = []
+def annotate_level(tokens):
+    level_annotations = []
     for token in tokens:
         token_text = token.text
         level, level_code = check_level(token_text)
         if level:
-            level_annotated.append((token_text, level, level_code))
+            level_annotations.append((token_text, level, level_code))
         else:
-            level_annotated.append(token_text)
-    return level_annotated
+            level_annotations.append(token_text)
+    return level_annotations
 
-level_assessment = []
-user_input = st.text_area("Enter text here", "")
-level_assessment = assess_level(user_input)
+def annotate_segmentations(tokens):
+    segmentation_annotations = []
+    for token in tokens:
+        token_text = token.text
+        segmentation_annotations.append((token_text, '', '#FFF'))
+    return segmentation_annotations
 
-if st.button('Assess Level'):
+def annotate_pos(tokens):
+    pos_annotations = []
+    for token in tokens:
+        token_text = token.text
+        token_pos = token.pos
+        token_ann = pos_ann.get(token_pos, '')
+        if token_ann:
+            pos_annotations.append((token_text, token_pos, token_ann))
+        else:
+            pos_annotations.append(token_text)
+    return pos_annotations
+
+def get_annotated_text(annotated_tokens):
     out = div()
-
-    for word in level_assessment:
+    for word in annotated_tokens:
         if isinstance(word, str):
             out(html.escape(word))
 
@@ -77,5 +97,31 @@ if st.button('Assess Level'):
 
     st.markdown(str(out), unsafe_allow_html=True)
 
+level_annotations = []
+segmentation_annotations = []
+pos_annotations = []
+user_input = st.text_area("Enter text here", "")
+tokens = get_tokens(user_input)
+level_annotations = annotate_level(tokens)
+segmentation_annotations = annotate_segmentations(tokens)
+pos_annotations = annotate_pos(tokens)
+
+
+col1, col2, col3 = st.beta_columns(3)
+with col1:
+    assess_level_flag = st.button('Assess Level')
+with col2:
+    seg_flag = st.button('Get Segmentation')
+with col3:
+    pos_flag = st.button('Get PosTag')
+
+if assess_level_flag:
+    get_annotated_text(annotated_tokens=level_annotations)
+
+if seg_flag:
+    get_annotated_text(annotated_tokens=segmentation_annotations)
+
+if pos_flag:
+    get_annotated_text(annotated_tokens=pos_annotations)
 
 # ང་བོད་ལ་འགྲོ་ནས་སླེད་པ་ཡིན་ སེམས་ཅན་ཐམས་ཅད་ལ་བཀྲ་ཤིས་བདེ་ལེགས་ཞུས་རྒྱུ་ཡིན་ སེམས་པ་སྐྱིད་པོ་འདུག།
